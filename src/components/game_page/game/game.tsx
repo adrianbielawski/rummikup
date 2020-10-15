4ab1fb238290d56b6b963e34082cd8bed013beda
Adrian Bielawski <adrian@bielaw.ski>
2020-10-15 19:48:48 +0100

    Circle cuntdown component added

--- src/components/game_page/game/game.tsx
+++ src/components/game_page/game/game.tsx
@@ -8,12 +8,15 @@ import Timer from './timer/timer'
 //Redux
 import { switchPlayer, finishRound } from 'store/actions/appActions'
 import { useTypedSelector } from 'store/reducers/index'
+import CircleCountdown from 'components/global_components/circle_countdown/circleCountdown'
 
 type HandleFinishRound = () => void
 
 const Game = () => {
     const dispatch = useDispatch()
     const players = useTypedSelector(state => state.app.players)
+    const timeLimit = useTypedSelector(state => state.app.timeLimit)
+    const timeLeft = useTypedSelector(state => state.app.timeLeft)
     const currentPlayer = useTypedSelector(state => state.app.currentPlayer)
     const colors = players[currentPlayer].color
     
@@ -32,6 +35,12 @@ const Game = () => {
             style={{ backgroundColor: colors[0] }}
         >
             <Timer color={colors[1]}/>
+            <CircleCountdown
+                time={timeLimit}
+                color={colors[1]}
+                radius={100}
+                innerRadius={30}
+                restart={timeLeft === timeLimit} />
             <p
                 className={styles.playerName}
                 style={{ color: colors[1] }}
---
+++ src/components/global_components/circle_countdown/circleCountdown.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+
+type ArcData = {
+    endAngle: number,
+    outerRadius: number,
+}
+
+type Props = {
+    color: string,
+    radius: number,
+    innerRadius: number,
+    zoomFactor: number,
+    period: number,
+    periodStart: number,
+    spikiness: number,
+    time: number,
+    restart: boolean,
+}
+
+const CircleCountdown = (props: Props) => {
+    const pathRef = useRef<SVGPathElement>(null)
+    const arcRef = useRef<d3.Selection<SVGPathElement | null, ArcData, null, undefined> | null>(null)
+
+    const arc = d3.arc<any, ArcData>()
+        .startAngle(0)
+        .innerRadius(props.innerRadius)
+
+    useEffect(() => {
+        arcRef.current = d3.select(pathRef.current)
+            .datum({ endAngle: 0, outerRadius: props.radius })
+            .attr('d', arc)
+
+        return () => {
+            d3.select(pathRef.current).remove()
+        }
+    }, [])
+
+    useEffect(() => {
+        if (props.restart === false) {
+            return
+        }
+        arcRef.current!.transition()
+            .ease(d3.easeLinear)
+            .duration(props.time * 1000)
+            .attrTween('d', data => {
+                const interpolate = d3.interpolate(0, 2 * Math.PI)
+                return t => {
+                    data.endAngle = interpolate(t)
+                    // Magic. Don't touch.
+                    const period = props.period
+                    const timeLeft = (1 - t) * props.time
+                    const spikiness = props.spikiness
+                    if (timeLeft <= props.periodStart + period / 2) {
+                        data.outerRadius = (
+                            1 + Math.pow(
+                                Math.E,
+                                Math.abs(
+                                    spikiness * (
+                                        (timeLeft % period) - period / 2
+                                    )
+                                ) - spikiness * period / 2
+                            ) * props.zoomFactor
+                        ) * props.radius
+                    }
+                    // End of magic. Keep working.
+                    else {
+                        data.outerRadius = props.radius
+                    }
+                    
+                    return arc(data)!
+                }
+            })
+    }, [props.restart])
+
+    const radius = props.radius * (1 + props.zoomFactor)
+
+    return (
+        <svg width={radius * 2} height={radius * 2}>
+            <g transform={`translate(${radius}, ${radius})`}>
+                <path ref={pathRef} fill={props.color} />
+            </g>
+        </svg>
+    )
+}
+
+CircleCountdown.defaultProps = {
+    radius: 50,
+    innerRadius: 0,
+    zoomFactor: .2,
+    period: 1,
+    periodStart: 10,
+    spikiness: 15,
+}
+
+export default CircleCountdown
